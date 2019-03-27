261c3ffa38d3d8bfce750e774f5b0f8d771e6a19
hamouziGAMERS <47302859+hamouziGAMERS@users.noreply.github.com>
2019-03-27 17:06:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ client.on('message', async msg => { // eslint-disable-line
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 //by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-					.setFooter("3bker Community ©")
+					.setFooter("Ô² , B💎SS.♕ ، ✩#0007")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
